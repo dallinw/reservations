3904ed932f31ab043d178889e27f60dafce1ee67
Dallin Wright <dallinwright01@gmail.com>
2022-11-07 13:03:12 +0100

    working db tests for carriers

--- src/libs/database/carriers.rs
+++ src/libs/database/carriers.rs
@@ -119,6 +119,39 @@ pub async fn find(
     Ok(carriers)
 }
 
+pub async fn delete(
+    transaction: &Transaction<'_>,
+    abbreviation: &String,
+) -> Result<Vec<Carrier>, tokio_postgres::Error> {
+    // Insert into the Carrier table
+    let query = r#"
+        DELETE
+        FROM
+            carriers
+        WHERE
+            carriers.abbreviation = ($1);
+    "#;
+
+    let statement: Statement = transaction.prepare_cached(query).await.unwrap();
+
+    let raw_carrier_query = transaction.query(&statement, &[
+        abbreviation,
+    ]).await;
+
+    let rows = match raw_carrier_query {
+        Ok(value) => value,
+        Err(error) => {
+            log::error!("{:#?}", error);
+
+            return Err(error);
+        }
+    };
+
+    let carriers: Vec<Carrier> = parse_results(rows);
+
+    Ok(carriers)
+}
+
 #[cfg(test)]
 mod tests {
     use actix_web::web::Data;
@@ -143,7 +176,7 @@ mod tests {
             .map(char::from)
             .collect();
 
-        let create_outcome = create(
+        let outcome = create(
             &transaction,
             &format!("test_{}", random_name),
             &format!("test_{}", random_name),
@@ -152,7 +185,7 @@ mod tests {
         // Commits only needed for integration tests when we must persist data
         // let _commit = transaction.commit().await;
 
-        match create_outcome {
+        match outcome {
             Ok(_) => {
                 log::debug!("Passed create, but should have failed!");
             }
@@ -170,7 +203,7 @@ mod tests {
         let mut client: Client = app_state.database_pool.get().await.unwrap();
         let transaction: Transaction = client.transaction().await.unwrap();
 
-        let create_outcome = create(
+        let outcome = create(
             &transaction,
             &"DELTA".to_string(),
             &"DA".to_string(),
@@ -179,13 +212,78 @@ mod tests {
         // Commits only needed for integration tests when we must persist data
         // let _commit = transaction.commit().await;
 
-        match create_outcome {
+        match outcome {
             Ok(_) => {
                 log::debug!("Passed create, but should have failed!");
             }
             Err(error) => {
                 log::info!("{:#?}", error);
                 panic!("Test successful");
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_delete() {
+        let app_state: Data<AppState> = Data::new(create_app_state().await);
+        let mut client: Client = app_state.database_pool.get().await.unwrap();
+        let transaction: Transaction = client.transaction().await.unwrap();
+
+        let random_name: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(48)
+            .map(char::from)
+            .collect();
+
+        let fullname = format!("test_{}", random_name);
+
+        let outcome = create(
+            &transaction,
+            &fullname,
+            &fullname,
+        ).await;
+
+        let outcome = delete(
+            &transaction,
+            &fullname,
+        ).await;
+
+        // Commits only needed for integration tests when we must persist data
+        // let _commit = transaction.commit().await;
+
+        match outcome {
+            Ok(_) => {
+                log::debug!("Passed create, but should have failed!");
+            }
+            Err(error) => {
+                log::info!("{:#?}", error);
+            }
+        }
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn test_delete_should_fail() {
+        let app_state: Data<AppState> = Data::new(create_app_state().await);
+        let mut client: Client = app_state.database_pool.get().await.unwrap();
+        let transaction: Transaction = client.transaction().await.unwrap();
+
+        let outcome = delete(
+            &transaction,
+            &"DA".to_string(),
+        ).await;
+
+
+        // Commits only needed for integration tests when we must persist data
+        // let _commit = transaction.commit().await;
+
+        match outcome {
+            Ok(_) => {
+                log::debug!("Passed create, but should have failed!");
+            }
+            Err(error) => {
+                log::info!("{:#?}", error);
+                panic!("Passed test that should fail");
             }
         }
     }
